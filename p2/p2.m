630d74f238b0115231e71ed387d08ab445461366
Wang Xinlu <wxlms@outlook.com>
2016-04-15 21:29:20 +0800

    init

--- p2/p2.m
+++ p2/p2.m
@@ -16,8 +16,8 @@ n3=min(n1) : max(n1)+size(n2,2)-1;
 %select the middle of pattern
 %figure;stem(n3-1,res);
 
-A=imread('./findsmiley.jpg');
-%A=imread('./findsmiley_t.jpg');
+%A=imread('./findsmiley.jpg');
+A=imread('./findsmiley_teacherGiven.jpg');
 A=double(A);
 A=A-255/2;
 
@@ -29,19 +29,23 @@ B=not([     0,0,0,0,0,0,0;
        0,1,0,0,0,1,0;
        0,1,1,1,1,1,0;
        0,0,0,0,0,0,0;
-]);% white --1; black --0;
-%imshow(B);
+]);
+% add a NOT to compensate mistake!
+% In fact, white--1--255; black --0;
+imshow(B);
 B=B-1/2;
-B=B';
+B=B*225/2;
+B=rot90(B,2);
+
 C=conv2(A,B);
 
 [maxval,I]=max(C(:));
 [row,col]=find(C==maxval);
 
-row=row-3
+row=row-5
 col=col-4
+imcrop(A,[row-3,col-3,6,7])
 
-
-
-
+%test1=A(row-3:row+3,col-3:col+4);
+%test2=A(row-4:row+2,col-2:col+3);
 
