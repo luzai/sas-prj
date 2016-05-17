f24cec5ea3344cb51f875c9f81da8d8e68346a74
Wang Xinglu <wxlms@outlook.com>
2016-05-17 14:26:56 +0800

    rm

--- p3/NoteCreate.m
+++
@@ -1,8 +0,0 @@
-function [out]=NoteCreate(frq,dur,type)
-if strcmp(type,'sin')
-    out =  sin(2*pi* [1:dur]/8000 * frq );
-elseif strcmp(type, 'square')
-    out =  (2*pi* [1:dur]/8000 * frq );
-elseif strcmp(type,'sawtooth')
-    out =sawtooth(2*pi* [1:dur]/8000 * frq );
-end
